51e99e644600fe4117a0bd7f69392f70b06117e0
pulkitshar611-web <pulkitshar611@gmail.com>
2026-03-05 22:12:41 -0800

    hj

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173', "https://electricity-billing.kiaantechnology.com",
     credentials: true
 }));
 app.use(express.json());
